92b87aa5f00b26c44a325d6122fe733d1774aa9d
Kanginsu <dlstnehfh@naver.com>
2022-07-23 16:53:41 +0900

    수정본

--- src/main/java/com/example/comento/Controller/Good.java
+++ src/main/java/com/example/comento/Controller/Good.java
@@ -1,22 +1,28 @@
+package com.example.comento.Controller;
+
 import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "Comento_Board")
+
 @ToString
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+
+
+@Entity
+@Table(name = "BOARD1234")
 public class Good {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-    private String view;
-    private String hello;
+    private String email;
+    private String pw;
+    private String views;
+
 
 }
--- src/main/java/com/example/comento/Controller/HomeController.java
+++ src/main/java/com/example/comento/Controller/HomeController.java
@@ -1,3 +1,5 @@
+package com.example.comento.Controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
